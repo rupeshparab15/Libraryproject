f8c738dfeee3501cd9f9e483d663116a9b976e07
rupeshparab15 <121303672+rupeshparab15@users.noreply.github.com>
2022-12-23 21:05:22 +0530

    Rupesh
    library file

---
+++ index.php
@@ -0,0 +1,68 @@
+ 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Online Library Management System
+	</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<style type="text/css">
+	nav
+	{
+		float: right;
+		word-spacing: 30px;
+		padding: 20px;
+	}
+	nav li 
+	{
+		display: inline-block;
+		line-height: 80px;
+	}
+</style>
+</head>
+
+
+<body>
+	<div class="wrapper">
+		<header>
+		<div class="logo">
+			<img src="Images/2.png">
+			<h1 style="color: white;">ONLINE LIBRARY MANAGEMENT SYSTEM</h1>
+		</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">HOME</a></li>
+					<li><a href="books.php">BOOKS</a></li>
+					<li><a href="student_login.php">STUDENT-LOGIN</a></li>
+					<li><a href="admin_login.php">ADMIN-LOGIN</a></li>
+					<li><a href="feedback.php">FEEDBACK</a></li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+		<div class="sec_img">
+			<br><br><br>
+			<div class="box1">
+				<br><br><br><br>
+				<h1 style="text-align: center; font-size: 35px;color: white;">Welcom to library</h1><br><br>
+				<h1 style="text-align: center;font-size: 25px;color: white;">Opens at: 09:00 </h1><br>
+				<h1 style="text-align: center;font-size: 25px;color: white;">Closes at: 15:00 </h1><br>
+			</div>
+		</div>
+		</section>
+		<footer>
+			<p style="color:white;  text-align: center; ">
+				<br><br>
+				Email: Online.library@gmail.com <br>
+				Mobile: +88018********
+			</p>
+		</footer>
+	</div>
+</body>
+
+</html>
+index.html
+Displaying index.html.
